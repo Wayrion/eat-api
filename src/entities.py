e4f5be5e274e43169bb61eed08f4758666339e20
activivan <activivan.studios@gmail.com>
2025-07-17 10:47:05 +0200

    Update working hours in Garching

--- src/entities.py
+++ src/entities.py
@@ -171,7 +171,7 @@ class Canteen(ApiRepresentable, Enum):
         Location("Boltzmannstraße 19, Garching", 48.268132, 11.672263),
         422,
         "https://mensa.liste.party/api/",
-        OpenHours(("11:00", "14:30"), ("11:00", "14:30"), ("11:00", "14:30"), ("11:00", "14:30"), ("11:00", "14:00")),
+        OpenHours(("10:45", "14:15"), ("10:45", "14:15"), ("10:45", "14:15"), ("10:45", "14:15"), ("10:45", "14:15")),
     )
     MENSA_LEOPOLDSTR = (
         "Mensa Leopoldstraße",
@@ -300,7 +300,7 @@ class Canteen(ApiRepresentable, Enum):
         ),
         457,
         None,
-        OpenHours(("08:00", "16:00"), ("08:00", "16:00"), ("08:00", "16:00"), ("08:00", "16:00"), ("08:00", "15:00")),
+        OpenHours(("08:30", "16:30"), ("08:30", "16:30"), ("08:30", "16:30"), ("08:30", "16:30"), ("08:30", "15:00")),
     )
     STUCAFE_CONNOLLYSTR = (
         "StuCafé Connollystraße",
